0750f5f313852783a6001488219b1fa51699ac13
ElavarasanM5757 <68002269+ElavarasanM5757@users.noreply.github.com>
2021-05-25 01:25:48 -0700

    Update Sum of arrays using file.py

--- Sum of arrays using file.py
+++ Sum of arrays using file.py
@@ -1,24 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# In[1]:
-
-
-#Sum of array
-
-lst = []
-num = int(input("Enter the size of array: "))
-print("Enter array elements:")
-for i in range(num):
-    numbers = int(input())
-    lst.append(numbers)
-print("Sum:",sum(lst))
-
-
-# In[2]:
-
-
-#Sum of array in files
 #Sum of Array using Files
 
 lst = []
@@ -26,9 +6,6 @@ f = open('file.txt')
 text = f.read()
 lst = list(map(int,text.split(' ')))
 print("Sum:",sum(lst))
-
-
-# In[ ]:
 
 
 
