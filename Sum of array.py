46a05756d2f6b96076e799abe28adf728662ddf2
ElavarasanM5757 <68002269+ElavarasanM5757@users.noreply.github.com>
2021-05-25 01:19:24 -0700

    Add files via upload

---
+++ Sum of array.py
@@ -0,0 +1,18 @@
+
+
+#Sum of array
+
+lst = []
+num = int(input("Enter the size of array: "))
+print("Enter array elements:")
+for i in range(num):
+    numbers = int(input())
+    lst.append(numbers)
+print("Sum:",sum(lst))
+
+
+
+
+
+
+
